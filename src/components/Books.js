a3710bb04f4331aa269724fa76382d0f8abe50ac
Guilherme Salim <guisalim@Guilhermes-MacBook-Pro.local>
2018-03-03 15:35:31 -0300

    Bug Fix: books with no thumbnail won't break the application

--- src/components/Books.js
+++ src/components/Books.js
@@ -13,13 +13,12 @@ const Books = (props) => {
             return 'none'
         }
     }
-
     return (
         <li>
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover" onClick={(e) => bookDetails(book)}
-                        style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.thumbnail})` }}>
+                        style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks && imageLinks.thumbnail})` }}>
                     </div>
                     <div className="book-shelf-changer">
                         <select
